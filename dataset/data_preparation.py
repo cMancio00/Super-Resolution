e0b790651c04131a1eac5b856a2b2c04ba68f6e8
Christian Mancini <mancinichristian00@gmail.com>
2024-07-24 10:44:59 +0200

    Implemented the download function
    We can choose a specific dataset from caltech101 to download, discarting all the others

---
+++ dataset/data_preparation.py
@@ -0,0 +1,33 @@
+import torch as th
+from torchvision.datasets import Caltech101
+from shutil import move, rmtree
+import os
+
+
+def download(path : str, dataset : str):
+    """Download a specific dataset in a given folder
+
+    Args:
+        path (str): folder to download the dataset
+        dataset (str): dataset name
+
+    Raises:
+        ValueError: The dataset is not a valid option
+    """
+    if dataset not in ['airplanes']:
+        raise ValueError(f"Invalid dataset choise: {dataset}")
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    if not os.listdir(path):
+        Caltech101(path,target_type="category",download=True)
+        move(path + "/caltech101/101_ObjectCategories/" + dataset, path)
+        rmtree(path + "/caltech101")
+    else:
+        print(f"Dataset {dataset} already exists.")
+
+
+if __name__ == "__main__":
+    download("./data", "airplanes")
+
