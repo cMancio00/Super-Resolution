a56e0072ffd59d7ea426d008ce6a0f34447d1df9
Christian Mancini <mancinichristian00@gmail.com>
2024-07-31 16:18:49 +0200

    Implemented data splitting
    An example is shown in the notebook
    
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

--- dataset/data_preparation.py
+++ dataset/data_preparation.py
@@ -1,10 +1,11 @@
-import torch as th
+import torch
 from torchvision.datasets import Caltech101
 from shutil import move, rmtree
 import os
+from torch.utils.data import Dataset, Subset
 
 
-def download(path : str, dataset : str):
+def download(path: str, dataset: str):
     """Download a specific dataset in a given folder
 
     Args:
@@ -21,13 +22,34 @@ def download(path : str, dataset : str):
         os.makedirs(path)
 
     if not os.listdir(path):
-        Caltech101(path,target_type="category",download=True)
+        Caltech101(path, target_type="category", download=True)
         move(path + "/caltech101/101_ObjectCategories/" + dataset, path)
         rmtree(path + "/caltech101")
     else:
         print(f"Dataset {dataset} already exists.")
 
 
-if __name__ == "__main__":
-    download("./data", "airplanes")
+def split_dataset(dataset: Dataset, sizes: dict[str, float]) -> list[Subset]:
+    """
+        Splits a dataset into training, validation, and test sets based on the provided sizes.
 
+        Args:
+            dataset: The dataset to be split.
+            sizes: A dictionary containing the sizes of the training, validation, and test sets.
+                The keys must be 'train', 'validation', and 'test', and the values must sum up to 1.0.
+
+        Returns:
+            A list of three Subset objects, representing the training, validation, and test sets.
+
+        Raises:
+            ValueError: If the sizes dictionary does not contain the required keys or if the sizes do not sum up to 1.0.
+        """
+    required_keys = {'train', 'validation', 'test'}
+    if set(required_keys) != set(sizes.keys()):
+        raise ValueError(f"Dictionary of sizes must contain 'train', 'validation', and 'test' keys")
+    if not sum(sizes.values()) == 1.0:
+        raise ValueError(f"Sizes do not summ up to 1.0,but got {sum(sizes.values()):.2f}")
+    train_size = int(sizes["train"] * len(dataset))
+    validation_size = int(sizes["validation"] * len(dataset))
+    test_size = len(dataset) - train_size - validation_size
+    return torch.utils.data.random_split(dataset, [train_size, validation_size, test_size])
