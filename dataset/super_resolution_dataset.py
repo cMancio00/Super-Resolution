5d5bb4e1bf8c41d94ac332458216691e403a622b
Christian Mancini <mancinichristian00@gmail.com>
2024-08-05 16:23:37 +0200

    Added a default transformation toTensor()
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

--- dataset/super_resolution_dataset.py
+++ dataset/super_resolution_dataset.py
@@ -1,10 +1,11 @@
 import os
 from PIL import Image
 from torch.utils.data import Dataset
+import torchvision.transforms as transforms
 
 
 class SuperResolutionDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, transform=transforms.Compose([transforms.ToTensor()])):
         self.root_dir = root_dir
         self.transform = transform
         self.file_names = os.listdir(root_dir)
